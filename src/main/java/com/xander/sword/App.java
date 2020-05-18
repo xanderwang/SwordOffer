b18de2858df56fa6e7321a2f05b8cae6a35db5eb
XanderWang <420640763@qq.com>
2020-05-18 23:37:35 +0800

    init code

---
+++ src/main/java/com/xander/sword/App.java
@@ -0,0 +1,13 @@
+package com.xander.sword;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
